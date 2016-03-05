3d5c84536b91f14b6ac88eac37076162f0b9b70e
will <wilfredrthibodeau@gmail.com>
2016-03-05 15:38:45 -0500

    changed style.css

--- member/member_login.php
+++ member/member_login.php
@@ -6,30 +6,27 @@
 
     <!-- main content goes here -->
     <article class="main">
-      <h2>Article Title</h2>
-      <p>main content goes here</p>
-   <h1>Member Login</h1>
-    <form action="" method="post" id="aligned">
-        <input type="hidden" name="action" value="login">
+        <h2>Member Login</h2>
+        <form action="" method="post" id="aligned">
+            <input type="hidden" name="action" value="login">
 
-        <label>Username:</label>
-        <input type="text"   required="required" name="username" size="30">
-        <br>
+            <label>Username:</label>
+            <input type="text"   required="required" name="username" size="30">
+            <br>
 
-        <label>Password:</label>
-        <input type="password"  required="required"  name="password" size="30">
-        <br>
+            <label>Password:</label>
+            <input type="password"  required="required"  name="password" size="30">
+            <br>
 
-        <label>&nbsp;</label>
-        <input type="submit" value="Login">
-    </form>
-    <?php echo $error_message; ?>
-    <h2>Not a member?</h2>
-    <form action="" method="post">
-        <input type="hidden" name="action" value="register">
-        <input type="submit" value="Resister Here">
-    </form>   
-    
+            <label>&nbsp;</label>
+            <input type="submit" value="Login">
+        </form>
+        <?php echo $error_message; ?>
+        <h2>Not a member?</h2>
+        <form action="" method="post">
+            <input type="hidden" name="action" value="register">
+            <input type="submit" value="Resister Here">
+        </form>   
     </article><!-- end main article -->
 
     <!-- first sidebar goes here -->
